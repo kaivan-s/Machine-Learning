54e859e8ead75e14332ae7c98707bc8f40b2ea6c
kaivan shah <kaivanshah1663@gmail.com>
2017-12-13 15:25:23 +0530

    no message

--- classification / K-Nearest Neighbors (K-NN)/K_nearest_neighbours.py
+++ classification / K-Nearest Neighbors (K-NN)/K_nearest_neighbours.py
@@ -63,3 +63,24 @@ y_pred=classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm=confusion_matrix(Y_test,y_pred)
 
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+mp.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+            alpha = 0.75, cmap = ListedColormap(('white', 'black')))
+mp.xlim(X1.min(), X1.max())
+mp.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    mp.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+               c = ListedColormap(('red', 'green'))(i), label = j)
+mp.title('KNN(Test set)')
+mp.xlabel('Age')
+mp.ylabel('Estimated Salary')
+mp.legend()
+mp.show()
+
+
+
+
