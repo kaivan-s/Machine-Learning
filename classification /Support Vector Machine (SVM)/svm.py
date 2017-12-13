991400686485331a7f7a93fe15633e4d80944299
kaivan shah <kaivanshah1663@gmail.com>
2017-12-13 15:18:10 +0530

    SVM

---
+++ classification /Support Vector Machine (SVM)/svm.py
@@ -0,0 +1,67 @@
+""" SVM MODEL """
+
+""" It is also a classification model for bettter accuracy 
+Here i have used linear kernel but some other types are also there which can be used 
+for better accuracy !! 
+
+Graph is not compulsory it is just for visualisation purposes 
+black region is where the employee buys the car and green dot are the correct predictions
+that employee bought the car.
+
+The red dot in black region is the wrong prediction that actually that bought the car
+but our model predicted that he did not bought the car !! 
+
+You can cross verify with the confusion matrix !!!!
+
+Remember The classes and libraries used !!! 
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as mp
+import pandas as pd
+
+dataset=pd.read_csv('Social_Network_Ads.csv')
+X=dataset.iloc[:,[2,3]].values
+Y=dataset.iloc[:,4].values
+
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.25,random_state=0)
+
+
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.fit_transform(X_test)
+
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state=0)
+classifier.fit(X_train,Y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(Y_test,y_pred)
+
+
+#Not compulsory 
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+mp.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('white', 'black')))
+mp.xlim(X1.min(), X1.max())
+mp.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    mp.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+mp.title('SVM Linear (Test set)')
+mp.xlabel('Age')
+mp.ylabel('Estimated Salary')
+mp.legend()
+mp.show()
+
+
+
